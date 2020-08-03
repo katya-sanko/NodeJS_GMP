3cfd1372e77f03a4b69d97c511cd40cbb7b1d253
Sanko, Ekaterina <ekaterina_sanko@epam.com>
2020-08-03 15:37:46 +0300

    Added first task draft

---
+++ src/task1_1.js
@@ -0,0 +1,18 @@
+// console.log('hello world');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  let chunk;
+  // Use a loop to make sure we read all available data.
+  while ((chunk = process.stdin.read()) !== null) {
+    process.stdout.write(`${chunk.split('').reverse().join('')}\n`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('end');
+});
+
+// process.exit(1);
+// process.exitCode = 1;
